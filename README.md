985910ab59146b758a869978344942fa970ecb08
Martin Salles <salles.martin@gmail.com>
2017-06-30 14:01:27 +0200

    Init repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+
+Applying linear regression to determine the best equation for bike availabilities
+
---
+++ main.m
@@ -0,0 +1,4 @@
+fprintf('Loading data...\n');
+data = csvread('stations_features.csv');
+
+fprintf('Data loaded.\n');
